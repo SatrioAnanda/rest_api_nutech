0e65813b719d96d5d457ff2e19ed8cfe4f9020fd
Satrio Ananda Setiawan <satriosetiawan99@gmail.com>
2024-11-24 18:49:16 +0700

    update

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,11 @@
-import path from path;
+import path from "path";
 
-module.exports = {
-  entry: './src/main.js', 
+export default {
+  entry: "./src/main.js",
   output: {
-    filename: 'main.js',
-    path: path.resolve(__dirname, 'dist'),
+    filename: "bundle.js",
+    path: path.resolve("dist"),
   },
-  target: 'node', 
-  mode: 'production',
+  target: "node",
+  mode: "production",
 };
