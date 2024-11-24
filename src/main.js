d2662baa705ed9e207dcef58df7cfa6959c49639
Satrio Ananda Setiawan <satriosetiawan99@gmail.com>
2024-11-24 17:40:48 +0700

    change port

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 import { web } from "./application/web.js";
 import { logger } from "./application/logging.js";
 
-web.listen(3002, () => {
+web.listen(3001, () => {
   logger.info("Server running on port 3002");
 });
